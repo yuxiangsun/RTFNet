da4ac16dd1eb291178df975deafc93d9841da9e6
yuxiangsun <16552207+yuxiangsun@users.noreply.github.com>
2020-12-13 19:40:59 +0800

    Update MF_dataset.py

--- util/MF_dataset.py
+++ util/MF_dataset.py
@@ -1,19 +1,18 @@
-# Modified by Yuxiang Sun, Dec.5, 2020
+# By Yuxiang Sun, Dec. 13, 2020
 # Email: sun.yuxiang@outlook.com
 
-import os
-import torch
+import os, torch
 from torch.utils.data.dataset import Dataset
-from torch.utils.data import DataLoader
 import numpy as np
-from PIL import Image 
+import PIL
 
 class MF_dataset(Dataset):
 
     def __init__(self, data_dir, split, input_h=480, input_w=640 ,transform=[]):
         super(MF_dataset, self).__init__()
 
-        assert split in ['train', 'val', 'test', 'test_day', 'test_night', 'val_test'], 'split must be "train"|"val"|"test"|"test_day"|"test_night"|"val_test"'  # test_day, test_night
+        assert split in ['train', 'val', 'test', 'test_day', 'test_night', 'val_test', 'most_wanted'], \
+            'split must be "train"|"val"|"test"|"test_day"|"test_night"|"val_test"|"most_wanted"'  # test_day, test_night
 
         with open(os.path.join(data_dir, split+'.txt'), 'r') as f:
             self.names = [name.strip() for name in f.readlines()]
@@ -25,24 +24,21 @@ class MF_dataset(Dataset):
         self.transform = transform
         self.n_data    = len(self.names)
 
+
     def read_image(self, name, folder):
         file_path = os.path.join(self.data_dir, '%s/%s.png' % (folder, name))
-        image     = np.asarray(Image.open(file_path)) # (w,h,c)
-        #image.flags.writeable = True
+        image     = np.asarray(PIL.Image.open(file_path)) # (w,h,c)
         return image
 
-    def get_item(self, index):
+    def __getitem__(self, index):
         name  = self.names[index]
         image = self.read_image(name, 'images')
         label = self.read_image(name, 'labels')
         for func in self.transform:
             image, label = func(image, label)
-        image = np.asarray(Image.fromarray(image).resize((self.input_w, self.input_h)), dtype=np.float32).transpose((2,0,1))/255
-        label = np.asarray(Image.fromarray(label).resize((self.input_w, self.input_h)), dtype=np.int64)
+        image = np.asarray(PIL.Image.fromarray(image).resize((self.input_w, self.input_h)), dtype=np.float32).transpose((2,0,1))/255
+        label = np.asarray(PIL.Image.fromarray(label).resize((self.input_w, self.input_h), resample=PIL.Image.NEAREST), dtype=np.int64)
         return torch.tensor(image), torch.tensor(label), name
-
-    def __getitem__(self, index):
-        return self.get_item(index)
 
     def __len__(self):
         return self.n_data
